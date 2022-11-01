858f0c6a2d144b0aa42dcfbf7154d6461129288a
Agorgianitis Loukas <agorglouk@gmail.com>
2022-11-01 19:56:45 +0200

    Generate service project skeleton

---
+++ service/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to wg-link/service
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ service/test/wg_link/service_test.clj
@@ -0,0 +1,7 @@
+(ns wg-link.service-test
+  (:require [clojure.test :refer :all]
+            [wg-link.service :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
