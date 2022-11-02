a974b9da5a092a26eb906bea5abac7efcb85a775
Agorgianitis Loukas <agorglouk@gmail.com>
2022-11-02 23:29:11 +0200

    Add service skeleton

--- service/dev/user.clj
+++ service/dev/user.clj
@@ -2,7 +2,11 @@
   "Commonly used symbols for easy access in the Clojure REPL during development."
   (:require [clojure.repl :refer (apropos dir doc find-doc pst source)]
             [clojure.pprint :refer (pprint)]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [wg-link.service.core :refer [run-dev]]))
 
 (comment
   (pprint (str/trim "This line suppresses some clj-kondo warnings.")))
+
+; Run dev server
+(def dev-server (run-dev))
--- service/src/wg_link/service/core.clj
+++ service/src/wg_link/service/core.clj
@@ -1,12 +1,47 @@
 (ns wg-link.service.core
-  (:gen-class))
+  (:gen-class) ; for -main method in uberjar
+  (:require [io.pedestal.http :as http]
+            [io.pedestal.http.route :as route]))
 
-(defn greet
-  "Callable entry point to the application."
-  [data]
-  (println (str "Hello, " (or (:name data) "World") "!")))
+(defn hello-world
+  [request]
+  (let [name (get-in request [:params :name] "World")]
+    {:status 200 :body (str "Hello " name "!\n")}))
+
+(def routes
+  #{["/greet" :get `hello-world]})
+
+(def service {:env                 :prod
+              ::http/routes        routes
+              ::http/resource-path "/public"
+              ::http/type          :jetty
+              ::http/port          8080})
+
+(defn run-dev
+  "The entry point for dev"
+  [& _]
+  (println "\nCreating your [DEV] server...")
+  (-> service ;; Start with production configuration
+      (merge {:env :dev
+              ;; Do not block thread that starts web server
+              ::http/join? false
+              ;; Routes can be a function that resolve routes,
+              ;; we can use this to set the routes to be reloadable
+              ::http/routes #(route/expand-routes (deref #'routes))
+              ;; All origins are allowed in dev mode
+              ::http/allowed-origins {:creds true :allowed-origins (constantly true)}})
+      ;; Wire up interceptor chains
+      http/default-interceptors
+      http/dev-interceptors
+      http/create-server
+      http/start))
+
+;; This is an adapted service map, that can be started and stopped
+;; From the REPL you can call http/start and http/stop on this service
+(defonce runnable-service (http/create-server service))
 
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (greet {:name (first args)}))
+  "The entry point"
+  [& _]
+  (println "\nCreating your server...")
+  (http/start runnable-service))
