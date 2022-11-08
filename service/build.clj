f02c8ed08c729eff390005bf5fcf1730af227de1
Agorgianitis Loukas <agorglouk@gmail.com>
2022-11-08 18:59:33 +0200

    Add uber build target

--- service/build.clj
+++ service/build.clj
@@ -9,6 +9,12 @@
 (defn test "Run the tests." [opts]
   (bb/run-tests opts))
 
+(defn uber "Build the uberjar." [opts]
+  (-> opts
+      (assoc :lib lib :version version :main main)
+      (bb/clean)
+      (bb/uber)))
+
 (defn ci "Run the CI pipeline of tests (and build the uberjar)." [opts]
   (-> opts
       (assoc :lib lib :version version :main main)
