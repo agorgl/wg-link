d265dac747ade2f783f07b9b075e8c63d4527c30
Agorgianitis Loukas <agorglouk@gmail.com>
2022-11-09 22:55:11 +0200

    Add network initialization in dev startup

--- service/dev/user.clj
+++ service/dev/user.clj
@@ -3,10 +3,14 @@
   (:require [clojure.repl :refer (apropos dir doc find-doc pst source)]
             [clojure.pprint :refer (pprint)]
             [clojure.string :as str]
-            [wg-link.service.core :refer [run-dev]]))
+            [wg-link.service.core :refer [run-dev]]
+            [wg-link.service.db :as db]))
 
 (comment
   (pprint (str/trim "This line suppresses some clj-kondo warnings.")))
+
+; Init the network
+(db/init-network "wg0" "10.5.5.0/24" "somedomain.com" 56000)
 
 ; Run dev server
 (def dev-server (run-dev))
